f15c64ad49ff0d9f8b37437a6a1b795ffe35ba8f
koma5 <koma5@a3a00770-7fb3-48ac-ab25-46395ce9908f>
2011-01-19 22:04:03 +0000

    added.
    git-svn-id: http://subversion.assembla.com/svn/snitch/trunk@3 a3a00770-7fb3-48ac-ab25-46395ce9908f

---
+++ ajax.js
@@ -0,0 +1,116 @@
+function createAJAXobject() {
+	if (window.XMLHttpRequest)
+	{
+		xmlhttp= new XMLHttpRequest();
+	}
+	else
+	{
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+} // end createAJAXobject()
+
+
+function submitlogin()
+{
+	createAJAXobject();
+
+	var username = document.getElementById('username').value;
+	var password = document.getElementById('password').value;
+	var query = "?username=" + username + "&password=" + password
+	//alert("login.php" + query);
+	xmlhttp.open("GET","login.php" + query ,true);
+	xmlhttp.setRequestHeader("Pragma", "no-cache");
+	xmlhttp.setRequestHeader("Cache-Control", "must-revalidate");
+	xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+	xmlhttp.send();
+
+	xmlhttp.onreadystatechange=function()
+	{
+		if (xmlhttp.readyState==4 && xmlhttp.status==200)
+		{
+			document.getElementById("status").innerHTML=xmlhttp.responseText;
+			if (xmlhttp.responseText == 'logged in successfully!')
+			{
+				document.getElementById("submitbutton").innerHTML='<input type="button" onclick="submitlogout()" value="logout" />';
+			}
+		}
+	}
+
+} //end submitlogin()
+
+function submitlogout()
+{
+	createAJAXobject();
+
+	var username = document.getElementById('username').value;
+	var password = document.getElementById('password').value;
+	var query = "?username=" + username + "&password=" + password
+	//alert("login.php" + query);
+	xmlhttp.open("GET","logout.php" + query ,true);
+	xmlhttp.setRequestHeader("Pragma", "no-cache");
+	xmlhttp.setRequestHeader("Cache-Control", "must-revalidate");
+	xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+	xmlhttp.send();
+
+	xmlhttp.onreadystatechange=function()
+	{
+		if (xmlhttp.readyState==4 && xmlhttp.status==200)
+		{
+			document.getElementById("status").innerHTML=xmlhttp.responseText;
+			document.getElementById("main").innerHTML= "";
+			document.getElementById("submitbutton").innerHTML='<input type="button" onclick="submitlogin()" value="login" />';
+		}
+	}
+
+} //end submitlogout()
+
+
+function navigationSearch()
+{
+	createAJAXobject();
+
+	xmlhttp.open("GET","search.php",true);
+	xmlhttp.setRequestHeader("Pragma", "no-cache");
+	xmlhttp.setRequestHeader("Cache-Control", "must-revalidate");
+	xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+	xmlhttp.send();
+
+	xmlhttp.onreadystatechange=function()
+	{
+		if (xmlhttp.readyState==4 && xmlhttp.status==200)
+		{
+			document.getElementById("main").innerHTML=xmlhttp.responseText;
+		}
+	}
+} //end submitlogin()
+
+function letstype(str)
+{
+	createAJAXobject();
+
+	if (str.length==0)
+	{
+		xmlhttp.onreadystatechange=function()
+		{
+		if (xmlhttp.readyState==4 && xmlhttp.status==200)
+			{
+			document.getElementById("changing").innerHTML=xmlhttp.responseText;
+			}
+		}
+		xmlhttp.open("GET","get_ip_per_host.php",true);
+		xmlhttp.send();
+	}
+
+	if (str.length>0)
+	{
+		xmlhttp.onreadystatechange=function()
+		  {
+		  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+			{
+			document.getElementById("changing").innerHTML=xmlhttp.responseText;
+			}
+		  }
+		xmlhttp.open("GET","get_ip_per_host.php?hostname="+str,true);
+		xmlhttp.send();
+	}
+} //end letstype()
