4d33d411b112d072b5617c2f7982ecca4bd70d2c
koma5 <koma5@a3a00770-7fb3-48ac-ab25-46395ce9908f>
2011-01-23 10:51:01 +0000

    arvet.tar.gz deketed
    git-svn-id: http://subversion.assembla.com/svn/snitch/trunk@28 a3a00770-7fb3-48ac-ab25-46395ce9908f

--- insert_testdata_users_only.sql
+++ insert_testdata_users_only.sql
@@ -3,5 +3,4 @@ USE fifthch_snitch;
 INSERT INTO tUser (usrName, usrPassword, usrEmail)
 VALUES
 	('marco', MD5('1234'), 'marco@5th.ch'),
-	('sepp', MD5('seppspassword'), 'marcocuoco@bluewin.ch'),
 	('arvet', MD5('foxa'), 'marcocuoco@bluewin.ch');
